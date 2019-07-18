f1a75827a95723cb73880cb7e325777d470572c3
miraematt <matttootell@hotmail.com>
2019-07-18 12:26:49 +0100

    title big and add margin

--- src/components/owner-screens/ProfileScreen.js
+++ src/components/owner-screens/ProfileScreen.js
@@ -11,7 +11,8 @@ import {
 } from "react-native";
 import Image from "react-native-scalable-image";
 import MenuButton from "../MenuButton";
-import {getOwnerByOwnerId} from "../../Api"
+import { getOwnerByOwnerId } from "../../Api";
+import BurgerMenuHeader from "../BurgerMenuHeader";
 
 const INITIAL_STATE = {
   userExist: false,
@@ -22,7 +23,8 @@ const INITIAL_STATE = {
     "Whilst we sell fantastic modern & seasonal beers, wines & mixed drinks we also have loose leaf teas and locally sourced filtered coffee from Ancoats Coffee plus sandwiches, pastries, sweet & savory baked goods.",
   address: "Hanover St, Manchester M60 0AB, UK",
   venueName: "The Pilcrow Pub",
-  shortDescription: "We are a contemporary pub situated at Sadlers Yard, Manchester.",
+  shortDescription:
+    "We are a contemporary pub situated at Sadlers Yard, Manchester.",
   photoUri:
     "https://static1.squarespace.com/static/5437909ee4b02d632f5b2d5d/58d93902e4fcb5ad94d1f2e4/58d976766b8f5b87ea1852f5/1490646748896/DSC_9391.JPG?format=500w",
   latitude: 53.4868458,
@@ -35,11 +37,8 @@ export default class PromoScreen extends React.Component {
     Auth.currentAuthenticatedUser()
       .then(user => {
         getOwnerByOwnerId(user.username).then(ownerDetails => {
-          
-          this.setState({ownerDetails})
-
-        })
-       
+          this.setState({ ownerDetails });
+        });
       })
       .catch(err => console.log(err));
     //get owner profile from backend
@@ -57,40 +56,46 @@ export default class PromoScreen extends React.Component {
       longitude,
       loading
     } = this.state;
+    const { navigation } = this.props;
+    console.log(navigation);
     return (
-      <ScrollView style={{ backgroundColor: "#FDD96E" }}>
-        <MenuButton navigation={this.props.navigation} />
-        <View style={styles.container}>
-          <Text style={styles.textStyle}>{venueName}</Text>
-          <Text style={styles.title}>{shortDescription}</Text>
-          <Text style={styles.phone}>Tel: {phoneNumber}</Text>
-          <Image
-            width={Dimensions.get("window").width - 32}
-            source={{ uri: "https://static1.squarespace.com/static/5437909ee4b02d632f5b2d5d/58d93902e4fcb5ad94d1f2e4/58d976766b8f5b87ea1852f5/1490646748896/DSC_9391.JPG?format=500w" }}
-          />
-          <Text style={styles.title}>{longDescription}</Text>
-          <MapView
-            style={styles.map}
-            provider="google"
-            region={{
-              latitude: 53.4808,
-              longitude: -2.2426,
-              latitudeDelta: 0.03,
-              longitudeDelta: 0.02
-            }}
-          >
-            <Marker
-              coordinate={{
-                latitude: latitude,
-                longitude: longitude
+      <>
+        <BurgerMenuHeader navigation={navigation} />
+        <ScrollView style={{ backgroundColor: "#FDD96E" }}>
+          <View style={styles.container}>
+            <Text style={styles.textStyle}>{venueName}</Text>
+            <Text style={styles.title}>{shortDescription}</Text>
+            <Text style={styles.phone}>Tel: {phoneNumber}</Text>
+            <Image
+              width={Dimensions.get("window").width - 32}
+              source={{
+                uri:
+                  "https://static1.squarespace.com/static/5437909ee4b02d632f5b2d5d/58d93902e4fcb5ad94d1f2e4/58d976766b8f5b87ea1852f5/1490646748896/DSC_9391.JPG?format=500w"
               }}
-              title={venueName}
-              description={shortDescription}
             />
-          </MapView>
-          <Text style={styles.offer}>Previous offers</Text>
-        </View>
-      </ScrollView>
+            <Text style={styles.title}>{longDescription}</Text>
+            <MapView
+              style={styles.map}
+              provider="google"
+              region={{
+                latitude: 53.4808,
+                longitude: -2.2426,
+                latitudeDelta: 0.03,
+                longitudeDelta: 0.02
+              }}
+            >
+              <Marker
+                coordinate={{
+                  latitude: latitude,
+                  longitude: longitude
+                }}
+                title={venueName}
+                description={shortDescription}
+              />
+            </MapView>
+          </View>
+        </ScrollView>
+      </>
     );
   }
 }
@@ -100,14 +105,14 @@ const styles = StyleSheet.create({
     backgroundColor: "#FDD96E",
     alignItems: "center",
     justifyContent: "space-around",
-    marginLeft: 16,
-    marginRight: 16
+    marginHorizontal: 16,
+    marginBottom: 32
   },
   map: { marginTop: 10, alignSelf: "stretch", height: 300 },
 
   textStyle: {
-    marginTop: 50,
-    fontSize: 17,
+    marginTop: 32,
+    fontSize: 24,
     fontWeight: "bold",
     color: "#0468d4"
   },

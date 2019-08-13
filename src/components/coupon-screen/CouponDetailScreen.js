6c84025c984b0be6d9cd69221143c6ecc9e6183c
ricojamjar <rich@genericgreeting.co.uk>
2019-08-13 18:58:30 +0100

    passing durationInSeconds to dealcard

--- src/components/coupon-screen/CouponDetailScreen.js
+++ src/components/coupon-screen/CouponDetailScreen.js
@@ -9,14 +9,14 @@ export default function PromoScreen(props) {
   /*getting items from props*/
   const { navigation } = props;
 
-  const name = navigation.getParam("name", "No Name");
+  const venueName = navigation.getParam("venueName", "No Name");
   const venueImg = navigation.getParam("venueImg", "No Venue Image");
   const drink = navigation.getParam("drink", "No Drink");
   const price = navigation.getParam("price", "No Price");
   const quantity = navigation.getParam("quantity", "No Quantity");
   const type = navigation.getParam("type", "No Type");
   const couponID = navigation.getParam("couponID", "No Coupon ID");
-  const duration = navigation.getParam("duration");
+  const durationInSeconds = navigation.getParam("durationInSeconds");
 
   return (
     <View style={styles.container}>
@@ -26,12 +26,12 @@ export default function PromoScreen(props) {
       </View>
       <View style={styles.dealcard}>
         <DealCard
-          venueName={name}
+          venueName={venueName}
           drink={drink}
           price={price}
           quantity={quantity}
           type={type}
-          duration={duration}
+          durationInSeconds={durationInSeconds}
         />
       </View>
     </View>
